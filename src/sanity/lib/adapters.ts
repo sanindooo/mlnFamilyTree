fd7b673e406fc78cbb2196777ddbc242ea5fdad5
Stephen Anindo <stephen@granitemarketing.co.uk>
2025-12-06 23:59:17 +0000

    fix: correct @sanity/image-url imports and types

--- src/sanity/lib/adapters.ts
+++ src/sanity/lib/adapters.ts
@@ -1,6 +1,6 @@
 import { PortableTextBlock, toPlainText } from "@portabletext/react";
-import createImageUrlBuilder from "@sanity/image-url";
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { createImageUrlBuilder } from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url";
 import { client } from "../client";
 import {
 	Person,
